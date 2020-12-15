db6c1587b71cacd963e3d646da4e64a15d1a01d2
Randy Le <randyL2004@outlook.com>
2020-12-15 08:31:57 -0700

    a

--- SQLine/Application/AppCommandQuestions.cs
+++ SQLine/Application/AppCommandQuestions.cs
@@ -22,88 +22,102 @@ namespace SQLine.Application
         {
             if (mode == AppMode.ConnectedToServer)
             {
-                if (command == AppCommands.QUESTION)
-                {
-                    HelpMenu.ConnectedToServer();
-                }
-
-                if (command == AppCommands.QUESTION_DATABASES)
-                {
-                    App.ListCachedDatabases();
-                }
-
-                if (command == AppCommands.QUESTION_DATABASES_UPDATE)
-                {
-                    App.GetDatabases(AppCache.ServerName);
-                }
+                HandleConnectedToServer(command);
             }
 
             if (mode == AppMode.PendingConnection)
             {
-                if (command == AppCommands.QUESTION)
-                {
-                    HelpMenu.PendingConnection();
-                }
+                HandlePendingConnection(command);
             }
 
             if (mode == AppMode.UsingDatabase)
             {
-                if (command == AppCommands.QUESTION)
-                {
-                    HelpMenu.UsingDatabase();
-                }
-
-                if (command == AppCommands.QUESTION_DATABASES)
-                {
-                    App.ListCachedDatabases();
-                }
-
-                if (command == AppCommands.QUESTION_DATABASES_UPDATE)
-                {
-                    App.GetDatabases(AppCache.ServerName);
-                }
-
-                if (command == AppCommands.QUESTION_DATABASES_UPDATE)
-                {
-                    App.GetDatabases(AppCache.ServerName);
-                }
-
-                if (command.StartsWith(AppCommands.QUESTION_TABLE))
-                {
-                    if (AppCache.Tables.Count == 0)
-                    {
-                        App.GetTables();
-                    }
-
-                    if (command == AppCommands.QUESTION_TABLES_UPDATE)
-                    {
-                        App.GetTables();
-                        App.ListTables(string.Empty);
-                    }
-
-                    if (command.StartsWith(AppCommands.QUESTION_TABLE_SCHEMA))
-                    {
-                        string prefix = command.Replace(AppCommands.QUESTION_TABLE_SCHEMA, string.Empty).Trim();
-                        App.GetTableSchema(prefix);
-                        App.ShowTableSchema(prefix);
-                    }
-                    // show all tables
-                    else if (command == AppCommands.QUESTION_TABLE)
-                    {
-                        App.ListTables(string.Empty);
-                    }
-                    else
-                    {
-                        // show all tables with the specified prefix
-                        string prefix = command.Replace(AppCommands.QUESTION_TABLE, string.Empty).Trim();
-                        App.ListTables(prefix);
-                    }
-                }
+                HandleUsingDatabase(command);
             }
         }
         #endregion
 
         #region Private Methods
+        internal static void HandleUsingDatabase(string command)
+        {
+            if (command == AppCommands.QUESTION)
+            {
+                HelpMenu.UsingDatabase();
+            }
+
+            if (command == AppCommands.QUESTION_DATABASES)
+            {
+                App.ListCachedDatabases();
+            }
+
+            if (command == AppCommands.QUESTION_DATABASES_UPDATE)
+            {
+                App.GetDatabases(AppCache.ServerName);
+            }
+
+            if (command == AppCommands.QUESTION_DATABASES_UPDATE)
+            {
+                App.GetDatabases(AppCache.ServerName);
+            }
+
+            if (command.StartsWith(AppCommands.QUESTION_TABLE))
+            {
+                if (AppCache.Tables.Count == 0)
+                {
+                    App.GetTables();
+                }
+
+                if (command == AppCommands.QUESTION_TABLES_UPDATE)
+                {
+                    App.GetTables();
+                    App.ListTables(string.Empty);
+                }
+
+                if (command.StartsWith(AppCommands.QUESTION_TABLE_SCHEMA))
+                {
+                    string prefix = command.Replace(AppCommands.QUESTION_TABLE_SCHEMA, string.Empty).Trim();
+                    App.GetTableSchema(prefix);
+                    App.ShowTableSchema(prefix);
+                }
+                // show all tables
+                else if (command == AppCommands.QUESTION_TABLE)
+                {
+                    App.ListTables(string.Empty);
+                }
+                else
+                {
+                    // show all tables with the specified prefix
+                    string prefix = command.Replace(AppCommands.QUESTION_TABLE, string.Empty).Trim();
+                    App.ListTables(prefix);
+                }
+            }
+        }
+
+        internal static void HandlePendingConnection(string command)
+        {
+            if (command == AppCommands.QUESTION)
+            {
+                HelpMenu.PendingConnection();
+            }
+        }
+
+        internal static void HandleConnectedToServer(string command)
+        {
+            if (command == AppCommands.QUESTION)
+            {
+                HelpMenu.ConnectedToServer();
+            }
+
+            if (command == AppCommands.QUESTION_DATABASES)
+            {
+                App.ListCachedDatabases();
+            }
+
+            if (command == AppCommands.QUESTION_DATABASES_UPDATE)
+            {
+                App.GetDatabases(AppCache.ServerName);
+            }
+        }
         #endregion
 
     }
