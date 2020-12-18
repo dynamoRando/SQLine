84d944951ffb34ee178009216bf7c458bf4ba3f7
Randy Le <randyL2004@outlook.com>
2020-12-17 17:50:39 -0700

    a

--- SQLineGUI/Program.cs
+++ SQLineGUI/Program.cs
@@ -1,32 +1,16 @@
-﻿using System;
+﻿using SQLineGUI.UI;
+using System;
 using Terminal.Gui;
 
 namespace SQLineGUI
 {
     class Program
     {
+        static Window win;
         static void Main(string[] args)
         {
-            Application.Init();
-            var menu = new MenuBar(new MenuBarItem[] {
-            new MenuBarItem ("_File", new MenuItem [] {
-                new MenuItem ("_Quit", "", () => {
-                    Application.RequestStop ();
-                })
-            }),
-        });
-
-            var win = new Window("Hello")
-            {
-                X = 0,
-                Y = 1,
-                Width = Dim.Fill(),
-                Height = Dim.Fill() - 1
-            };
-
-            // Add both menu and win in a single call
-            Application.Top.Add(menu, win);
-            Application.Run();
+            AppLine.Init();
+          
         }
     }
 }
---
+++ SQLineGUI/UI/AppLine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+
+namespace SQLineGUI.UI
+{
+    public static class AppLine
+    {
+        static Window win;
+
+        public static void Init()
+        {
+            Application.Init();
+            var menu = new MenuBar(new MenuBarItem[] {
+            new MenuBarItem ("_File", new MenuItem [] {
+                new MenuItem ("_Quit", "", () => {
+                    Application.RequestStop ();
+                })
+            }),
+        });
+
+            win = new Window("asdf")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill() - 1
+            };
+
+            win.KeyDown += Win_KeyDown;
+
+            // Add both menu and win in a single call
+            Application.Top.Add(menu, win);
+            Application.Run();
+        }
+
+        private static void Win_KeyDown(View.KeyEventEventArgs obj)
+        {
+            win.Title = obj.KeyEvent.KeyValue.ToString();
+        }
+    }
+}
