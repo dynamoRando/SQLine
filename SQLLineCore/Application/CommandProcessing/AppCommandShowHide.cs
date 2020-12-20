96546c78bc9535c263bf6e14eb750f8245426776
Randy Le <randyL2004@outlook.com>
2020-12-19 19:40:06 -0700

    a

---
+++ SQLLineCore/Application/CommandProcessing/AppCommandShowHide.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLineCore.Application.CommandProcessing
+{
+    internal static class AppCommandShowHide
+    { 
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Public Methods
+        internal static void HandleHide(string command)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void HandleShow(string command)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+    }
+}
