631f66cd9ed1dcbe5a464fc1cffd721a59b05482
Randy Le <randyL2004@outlook.com>
2020-12-19 19:32:42 -0700

    formatting queries

--- SQLLineCore/Application/CommandProcessing/AppCommandQuery.cs
+++ SQLLineCore/Application/CommandProcessing/AppCommandQuery.cs
@@ -33,24 +33,50 @@ namespace SQLineCore.Application.CommandProcessing
                 var table = new DataTable();
                 adapter.Fill(table);
 
-                string columnsHeader = string.Empty;
+                var columns = new List<ColumnInfo>();
+                var columnNames = new List<string>();
+
+                int maxColumnNameLength = 0;
+
                 foreach (DataColumn column in table.Columns)
                 {
-                    columnsHeader += column.ColumnName;
+                    columnNames.Add(column.ColumnName);
+
+                    if (column.ColumnName.Length > maxColumnNameLength)
+                    {
+                        maxColumnNameLength = column.ColumnName.Length;
+                    }
+
+                    //MaxLength = column.MaxLength == -1 ? 10 : column.MaxLength,
+                    var col = new ColumnInfo()
+                    {
+                        ColumnName = column.ColumnName,
+                        MaxLength = column.MaxLength,
+                        DataType = column.DataType.ToString(),
+                        Ordinal = column.Ordinal
+                    };
+
+                    if (maxColumnNameLength > col.MaxLength)
+                    {
+                        col.MaxLength = maxColumnNameLength;
+                    }
+
+                    columns.Add(col);
                 }
 
-                result.Add(columnsHeader);
+                string formatter = BuildFormatter(columns);
+                result.Add(string.Format(formatter, columnNames.ToArray()));
 
-                string value = string.Empty;
+                var columnValues = new List<string>();
 
-                foreach(DataRow row in table.Rows)
+                foreach (DataRow row in table.Rows)
                 {
-                    value = string.Empty;
                     foreach (DataColumn col in table.Columns)
                     {
-                        value += row[col].ToString();
+                        columnValues.Add(row[col].ToString());
                     }
-                    result.Add(value);
+                    result.Add(string.Format(formatter, columnValues.ToArray()));
+                    columnValues.Clear();
                 }
             }
 
@@ -59,6 +85,47 @@ namespace SQLineCore.Application.CommandProcessing
         #endregion
 
         #region Private Methods
+        private static string BuildFormatter(List<ColumnInfo> columns)
+        {
+            StringBuilder result = new StringBuilder();
+            string prefix = string.Empty;
+
+            //  string formatter = "{0,-" + maxColLength.ToString() + "} {1,-10} {2,10} {3,-5}";
+            foreach (var column in columns)
+            {
+                prefix = string.Empty;
+
+                if (IsLeftAligned(column))
+                {
+                    prefix = "-";
+                }
+                else
+                {
+                    prefix = string.Empty;
+                }
+
+                result.Append("{" + column.Ordinal + "," + prefix + column.MaxLength.ToString() + "} ");
+            }
+
+            return result.ToString().Trim();
+        }
+
+        /// <summary>
+        /// Attempts to determine if the field is text. If yes, it will return true, otherwise false
+        /// </summary>
+        /// <param name="column">The column to align</param>
+        /// <returns>True if the column is text, otherwise false</returns>
+        private static bool IsLeftAligned(ColumnInfo column)
+        {
+            if (column.DataType.Contains("string", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         #endregion
 
     }
