cc62ab1b737ec838bfe63b0d448e367c75575ac1
Randy Le <randyl04@hey.com>
2020-12-26 13:01:02 -0700

    setup for settings

---
+++ SQLLineCore/Settings/AppSettings.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SQLineCore
+{
+    public class AppSettings
+    {
+        public List<ConnectionPreference> Connections {get; set;}
+    }
+}
