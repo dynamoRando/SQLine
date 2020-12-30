6f38d23ccaf6e98de8fd3f5414533a1b36d4f6cd
Randy Le <randyL04@hey.com>
2020-12-30 09:09:32 -0700

    refactor

---
+++ SQLLineCore/Application/CommandProcessing/AppCommandQuestionStoredProcedure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLineCore
+{
+    internal static class AppCommandQuestionStoredProcedure
+    {
+        #region Public Methods
+        #endregion
+    }
+}
---
+++ SQLLineCore/Application/CommandProcessing/AppCommandQustionView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLineCore
+{
+    internal static class AppCommandQustionView
+    {
+        #region Public Methods
+        #endregion
+    }
+}
