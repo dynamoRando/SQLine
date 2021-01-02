f60dcba22e457fae632cb8c2c903930f6427ea47
Randy Le <randyL2004@outlook.com>
2021-01-02 12:31:59 -0700

    Revert "refactor handle question table"
    This reverts commit 59df177c31be4db5bff1ee7788c155a22754079d.

--- SQLLineCore/Application/CommandProcessing/AppCommandQuestions.cs
+++ SQLLineCore/Application/CommandProcessing/AppCommandQuestions.cs
@@ -36,7 +36,9 @@ namespace SQLineCore
 
             return result;
         }
+        #endregion
 
+        #region Private Methods
         internal static List<string> HandleUsingDatabase(string command)
         {
             var result = new List<string>();
@@ -63,12 +65,62 @@ namespace SQLineCore
 
             if (command.StartsWith(AppCommands.QUESTION_TABLE))
             {
-                result = HandleQuestionTable(command);
+                if (AppCache.Tables.Count == 0)
+                {
+                    App.GetTables();
+                }
+
+                if (command == AppCommands.QUESTION_TABLES_UPDATE)
+                {
+                    App.GetTables();
+                    result = App.ListTables(string.Empty, string.Empty);
+                }
+
+                if (command.StartsWith(AppCommands.QUESTION_TABLE_SCHEMA))
+                {
+                    string prefix = command.Replace(AppCommands.QUESTION_TABLE_SCHEMA, string.Empty).Trim();
+                    App.GetTableSchema(prefix, string.Empty);
+                    result = App.ShowTableSchema(prefix);
+                }
+                // show all tables
+                else if (command == AppCommands.QUESTION_TABLE)
+                {
+                    result = App.ListTables(string.Empty, string.Empty);
+                }
+                else
+                {
+                    //? t -schema foo
+                    //? t -schema foo prefix
+                    if (command.Contains(AppCommands.SCHEMA, System.StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        string schemaName = string.Empty;
+                        string prefix = string.Empty;
+
+                        var array = command.Split(" ");
+                        var items = array.ToList();
+                        items.Remove(AppCommands.QUESTION);
+                        items.Remove("t");
+                        int index = items.IndexOf("-schema");
+                        schemaName = items[index + 1];
+                        if (items.Count() > index + 2)
+                        {
+                            prefix = items[index + 2];
+                        }
+
+                        result = App.ListTables(prefix, schemaName);
+                    }
+                    else
+                    {
+                        //? t prefix
+                        // show all tables with the specified prefix
+                        string prefix = command.Replace(AppCommands.QUESTION_TABLE, string.Empty).Trim();
+                        result = App.ListTables(prefix, string.Empty);
+                    }
+                }
             }
 
             return result;
         }
-
 
         internal static List<string> HandlePendingConnection(string command)
         {
@@ -98,68 +150,6 @@ namespace SQLineCore
             if (command == AppCommands.QUESTION_DATABASES_UPDATE)
             {
                 result = App.GetDatabases(AppCache.ServerName);
-            }
-
-            return result;
-        }
-        #endregion
-
-        #region Private Methods
-        private static List<string> HandleQuestionTable(string command)
-        {
-            var result = new List<string>();
-
-            if (AppCache.Tables.Count == 0)
-            {
-                App.GetTables();
-            }
-
-            if (command == AppCommands.QUESTION_TABLES_UPDATE)
-            {
-                App.GetTables();
-                result = App.ListTables(string.Empty, string.Empty);
-            }
-
-            if (command.StartsWith(AppCommands.QUESTION_TABLE_SCHEMA))
-            {
-                string prefix = command.Replace(AppCommands.QUESTION_TABLE_SCHEMA, string.Empty).Trim();
-                App.GetTableSchema(prefix, string.Empty);
-                result = App.ShowTableSchema(prefix);
-            }
-            // show all tables
-            else if (command == AppCommands.QUESTION_TABLE)
-            {
-                result = App.ListTables(string.Empty, string.Empty);
-            }
-            else
-            {
-                //? t -schema foo
-                //? t -schema foo prefix
-                if (command.Contains(AppCommands.SCHEMA, System.StringComparison.CurrentCultureIgnoreCase))
-                {
-                    string schemaName = string.Empty;
-                    string prefix = string.Empty;
-
-                    var array = command.Split(" ");
-                    var items = array.ToList();
-                    items.Remove(AppCommands.QUESTION);
-                    items.Remove("t");
-                    int index = items.IndexOf("-schema");
-                    schemaName = items[index + 1];
-                    if (items.Count() > index + 2)
-                    {
-                        prefix = items[index + 2];
-                    }
-
-                    result = App.ListTables(prefix, schemaName);
-                }
-                else
-                {
-                    //? t prefix
-                    // show all tables with the specified prefix
-                    string prefix = command.Replace(AppCommands.QUESTION_TABLE, string.Empty).Trim();
-                    result = App.ListTables(prefix, string.Empty);
-                }
             }
 
             return result;
