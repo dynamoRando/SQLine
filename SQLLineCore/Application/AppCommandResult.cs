1bd832aa72205dd0fafc600497b8bd3311b33b85
Randy Le <randyL2004@outlook.com>
2021-01-04 16:52:50 -0700

    documentation

--- SQLLineCore/Application/AppCommandResult.cs
+++ SQLLineCore/Application/AppCommandResult.cs
@@ -3,8 +3,16 @@ using System.Linq;
 
 namespace SQLineCore
 {
+    /// <summary>
+    /// Used to determine if the command will return a result that is text based or a data table
+    /// </summary>
     public static class AppCommandResult
     {
+        /// <summary>
+        /// Returns true if the app command supplied should return a text based result
+        /// </summary>
+        /// <param name="command">The app command</param>
+        /// <returns>True if the result should be text based, otherwise false</returns>
         public static bool IsText(string command)
         {
             bool result = false;
@@ -20,6 +28,12 @@ namespace SQLineCore
             return result;
         }
 
+
+        /// <summary>
+        /// Returns true if the app command supplied should return a data table result, otherwise false
+        /// </summary>
+        /// <param name="command">The app commmand</param>
+        /// <returns>True if the result should be data table based, otherwise false</returns>
         public static bool IsData(string command)
         {
             bool result = false;
