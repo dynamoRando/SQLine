32f50fef15e4a21a189a6eb9657e090ec6e965d7
Randy Le <randyL2004@outlook.com>
2021-01-05 10:04:53 -0700

    setup for getting and listing stored procedures

--- SQLLineCore/Application/AppStoredProcedureAction.cs
+++ SQLLineCore/Application/AppStoredProcedureAction.cs
@@ -6,5 +6,29 @@ namespace SQLineCore
 {
     internal static class AppStoredProcedureAction
     {
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Public Methods
+        internal static List<string> GetProcedures()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static List<string> ListProcedures()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+
     }
 }
